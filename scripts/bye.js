532da604914e0880ef806fce7d50ba6482ff9a30
Lehanchic25 <112811109+Lehanchic@users.noreply.github.com>
2023-03-20 23:51:42 +0300

    Delete bye.js

--- scripts/bye.js
+++
@@ -1,7 +0,0 @@
-const myDialog = new BaseDialog("Dialog Title");
-// Add "go back" button
-myDialog.addCloseButton();
-// Add text to the main content
-myDialog.cont.add("Goodbye!");
-// Show dialog
-myDialog.show();
